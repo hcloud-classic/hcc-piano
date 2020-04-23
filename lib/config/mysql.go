57916b096dc635719af2858d56e3e030fbbf4b50
cgs0730 <cgs0730@innogrid.com>
2020-04-23 10:34:14 +0900

    [Hcloud-MiddleWare] Piano Config Setting
    mysql, influxDB config

---
+++ lib/config/mysql.go
@@ -0,0 +1,10 @@
+package config
+
+// MysqlID : MySQL server login id
+var MysqlID = "root"
+
+// MysqlPort : MySQL server port number
+var MysqlPort = "3306"
+
+// MysqlDatabase : MySQL server database name of module
+var MysqlDatabase = "piano"
